d895c4be2637cd7b7383560d1adaaebdae8b7875
Narukarudra09 <160457532+Narukarudra09@users.noreply.github.com>
2025-02-08 21:43:23 +0530

    commit

--- lib/screens/therapist_screen/therapist_daily_data_screen.dart
+++ lib/screens/therapist_screen/therapist_daily_data_screen.dart
@@ -166,7 +166,7 @@ class TherapistDailyDataScreen extends StatelessWidget {
               ListView.builder(
                 physics: NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
-                itemCount: 4,
+                itemCount: 5,
                 itemBuilder: (context, index) {
                   return Container(
                     margin: const EdgeInsets.only(bottom: 16),
@@ -189,98 +189,23 @@ class TherapistDailyDataScreen extends StatelessWidget {
                                 fontWeight: FontWeight.w500,
                                 color: Color(0xFF080C3E)),
                           ),
-                          // Tags row
                           Wrap(
                             spacing: 8,
                             runSpacing: 8,
                             children: [
-                              Container(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 12,
-                                  vertical: 6,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: Color.fromARGB(255, 233, 233, 233),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: FittedBox(
-                                  fit: BoxFit.scaleDown,
-                                  child: Text(
-                                    "Thymus + Chest",
-                                    style: GoogleFonts.inter(
-                                      color: Color.fromARGB(255, 46, 44, 52),
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ),
-                              ),
+                              TherapyTag(text: 'Thymus + Chest'),
+                              TherapyTag(text: 'Thymus + Chest'),
                             ],
                           ),
                           const SizedBox(height: 16),
                           // Date row
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Flexible(
-                                child: Text(
-                                  "Date",
-                                  style: GoogleFonts.inter(
-                                    color: Color.fromARGB(255, 55, 61, 69),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                              const SizedBox(width: 8),
-                              Flexible(
-                                flex: 2,
-                                child: Text(
-                                  "${DateTime.now().day.toString()}-${DateTime.now().month.toString()}-${DateTime.now().year} ${DateTime.now().hour.toString()}:${DateTime.now().minute.toString()}:${DateTime.now().second.toString()}",
-                                  style: GoogleFonts.inter(
-                                    color: Color.fromARGB(255, 147, 158, 170),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.right,
-                                ),
-                              ),
-                            ],
+                          _buildInfoRow(
+                            'Date',
+                            "${DateTime.now().day.toString()}-${DateTime.now().month.toString()}-${DateTime.now().year} ${DateTime.now().hour.toString()}:${DateTime.now().minute.toString()}:${DateTime.now().second.toString()}",
                           ),
                           const SizedBox(height: 8),
                           // Given by row
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Flexible(
-                                child: Text(
-                                  "Given by",
-                                  style: GoogleFonts.inter(
-                                    color: Color.fromARGB(255, 55, 61, 69),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                              const SizedBox(width: 8),
-                              Flexible(
-                                flex: 2,
-                                child: Text(
-                                  "Ankit",
-                                  style: GoogleFonts.inter(
-                                    color: Color.fromARGB(255, 147, 158, 170),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.right,
-                                ),
-                              ),
-                            ],
-                          ),
+                          _buildInfoRow('Given by', 'Ankit'),
                         ],
                       ),
                     ),
@@ -288,6 +213,73 @@ class TherapistDailyDataScreen extends StatelessWidget {
                 },
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildInfoRow(String label, String value) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Flexible(
+        child: Text(
+          label,
+          style: GoogleFonts.inter(
+            color: Color.fromARGB(255, 55, 61, 69),
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      const SizedBox(width: 8),
+      Flexible(
+        flex: 2,
+        child: Text(
+          value,
+          style: GoogleFonts.inter(
+            color: Color.fromARGB(255, 147, 158, 170),
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+          ),
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.right,
+        ),
+      ),
+    ],
+  );
+}
+
+class TherapyTag extends StatelessWidget {
+  final String text;
+
+  const TherapyTag({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 12,
+        vertical: 6,
+      ),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 233, 233, 233),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: FittedBox(
+        fit: BoxFit.scaleDown,
+        child: Text(
+          text,
+          style: GoogleFonts.inter(
+            color: Color.fromARGB(255, 46, 44, 52),
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
           ),
         ),
       ),
